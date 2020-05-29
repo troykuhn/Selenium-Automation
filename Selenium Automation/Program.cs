d2f56f3f9b434d891472e837625d0055324daabd
Troy Kuhn <troykuhn22@gmail.com>
2020-05-29 10:38:26 -0400

    adding NUnit and Selenium nuget packages

--- Selenium Automation/Program.cs
+++ Selenium Automation/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,25 @@ namespace Selenium_Automation
 	{
 		static void Main( string[] args )
 		{
+			// Create a reference for Chrome browser
+			IWebDriver driver = new ChromeDriver();
+
+			// Go to Google Webpage
+			driver.Navigate().GoToUrl( "https://www.google.com" );
+
+			//Full screen browser
+			driver.Manage().Window.Maximize();
+
+			// Find the elements
+			IWebElement searchBox = driver.FindElement( By.Name( "q" ) );
+
+			// Enter something into the search bar
+			searchBox.SendKeys( "Automation practice - Selenium Sample" );
+
+			//Close the browser
+			driver.Quit();
+
+
 		}
 	}
 }
